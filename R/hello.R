9a935c7a36e786fec86471f9a4e65786ee6ed6cb
Joao-Formigari <137228301+Joao-Formigari@users.noreply.github.com>
2023-06-22 15:21:55 -0300

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -1,19 +1,19 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+library(yfR)
 
-hello <- function() {
-  print("Hello, world!")
-}
-hello()
+nome_acao <- "GOOG"   # Codigo no Yahoo Finance
+data_ini  <- "2010-01-01" # Data de inicio
+data_fim  <- "2023-06-20" # Data de fim
+precos <- yf_get(tickers = nome_acao, first_date = data_ini, last_date = data_fim)
+fechamento <- precos$price_adjusted
+
+spec <- ugarchspec(mean.model = list(armaOrder = c(3, 1), include.mean = FALSE),
+                     variance.model = list(model = 'sGARCH', garchOrder = c(1, 1)),
+                     distribution = 'norm')
+  
+fit_01 <- ugarchfit(spec, fechamento, solver = 'hybrid')
+  
+previsao <- ugarchforecast(fit_01, fechamento, n.ahead = 1)
+
+write_csv(as.data.frame(list("previsao"=previsao@forecast[["seriesFor"]],
+                             "sigma"=previsao@forecast[["sigmaFor"]])),paste0('data/',Sys.Date(),'previsao','.csv'))    
+
