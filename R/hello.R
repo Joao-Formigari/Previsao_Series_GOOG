9a5946e45c1a06493bd2797d4c63e2eaf155ed2f
Joao-Formigari <137228301+Joao-Formigari@users.noreply.github.com>
2023-06-25 00:06:00 -0300

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -9,7 +9,7 @@ data_fim <- "2023-06-23"
 precos <- yf_get(tickers = nome_acao, first_date = data_ini, last_date = data_fim)
 fechamento <- as.data.frame(list(precos$price_adjusted, precos$ref_date))
 
-spec <- ugarchspec(mean.model = list(armaOrder = c(3, 1), include.mean = FALSE),
+spec <- ugarchspec(mean.model = list(armaOrder = c(2, 1), include.mean = FALSE),
                      variance.model = list(model = 'sGARCH', garchOrder = c(1, 1)),
                      distribution = 'norm')
   
@@ -17,14 +17,15 @@ previsao_antes <- read.csv("https://raw.githubusercontent.com/Joao-Formigari/Tes
 
 fechamento[nrow(fechamento)+1,] <- previsao_antes[nrow(previsao_antes),c(1,3)]
 
-fit_01 <- ugarchfit(spec, fechamento[-3391,], solver = 'hybrid')
+fit_01 <- ugarchfit(spec, fechamento$fechamento, solver = 'hybrid')
 
-previsao <- ugarchforecast(fit_01, fechamento[-3391], n.ahead = 1)
+previsao <- ugarchforecast(fit_01, fechamento$fechamento,
+                           n.ahead = nrow(previsao_antes))[nrow(previsao_antes),]
+
 
 previsao_antes[,3]<-as.Date(previsao_antes[,3])
 previsao_antes[nrow(previsao_antes)+1,1] <- previsao@forecast[["seriesFor"]]
 previsao_antes[nrow(previsao_antes),2] <- previsao@forecast[["sigmaFor"]]
 previsao_antes[nrow(previsao_antes),3] <- as.Date(previsao@model$modeldata$index[length(previsao@model$modeldata$index)])+nrow(previsao_antes)-1
 
-write_csv(previsao_antes,paste0('data/','previsao','.csv'))   
-
+write_csv(previsao_antes,paste0('data/','previsao','.csv'))  
