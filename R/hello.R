644b7f88b43b0845b85f9f97c43ae7af29c4a5c8
Joao-Formigari <137228301+Joao-Formigari@users.noreply.github.com>
2023-06-26 07:26:40 -0300

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -22,11 +22,11 @@ if (nrow(previsao_antes) > 0){
   fechamento[,3] <- as.Date(fechamento$data)
   fit_01 <- ugarchfit(spec, fechamento$fechamento[-3391], solver = 'hybrid')
   
-  previsao <- ugarchforecast(fit_01, fechamento$fechamento[-3391],
-                             n.ahead = nrow(previsao_antes))[nrow(previsao_antes),]
+  previsao <- ugarchforecast(fit_01, fechamento[-3391],
+                             n.ahead = nrow(previsao_antes)+1)
   previsao_antes[,3]<-as.Date(previsao_antes[,3])
-  previsao_antes[nrow(previsao_antes)+1,1] <- previsao@forecast[["seriesFor"]]
-  previsao_antes[nrow(previsao_antes),2] <- previsao@forecast[["sigmaFor"]]
+  previsao_antes[nrow(previsao_antes)+1,1] <- previsao@forecast[["seriesFor"]][nrow(previsao_antes)]
+  previsao_antes[nrow(previsao_antes),2] <- previsao@forecast[["sigmaFor"]][nrow(previsao_antes)]
   previsao_antes[nrow(previsao_antes),3] <- fechamento$data[nrow(fechamento)]+nrow(previsao_antes)-1
 }
 
