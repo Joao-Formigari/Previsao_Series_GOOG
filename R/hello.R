c7694524e1dde5024b6ff9ac1d35cfa7ef6233a5
Joao-Formigari <137228301+Joao-Formigari@users.noreply.github.com>
2023-06-26 14:48:48 -0300

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -5,10 +5,10 @@ library(scales)
 
 nome_acao <- "GOOG"   # Codigo no Yahoo Finance
 data_ini  <- "2010-01-01" # Data de inicio
-data_fim <- "2023-06-23"
+data_fim <- Sys.Date()
 precos <- yf_get(tickers = nome_acao, first_date = data_ini, last_date = data_fim)
-fechamento <- as.data.frame(list(precos$price_adjusted, precos$ref_date))
-colnames(fechamento) <- c("fechamento","data")
+retorno <- as.data.frame(list(precos$ret_adjusted_prices[-1], precos$ref_date))
+colnames(retorno) <- c("retorno","data")
 
 spec <- ugarchspec(mean.model = list(armaOrder = c(2, 1), include.mean = FALSE),
                      variance.model = list(model = 'sGARCH', garchOrder = c(1, 1)),
@@ -17,28 +17,28 @@ spec <- ugarchspec(mean.model = list(armaOrder = c(2, 1), include.mean = FALSE),
 previsao_antes <- read.csv("https://raw.githubusercontent.com/Joao-Formigari/Teste/master/data/previsao.csv")
 
 if (nrow(previsao_antes) > 0){
-  fechamento[nrow(fechamento)+1,1] <- previsao_antes[nrow(previsao_antes),1]
-  fechamento[nrow(fechamento),2] <- as.Date(previsao_antes[nrow(previsao_antes),3])
-  fechamento[,3] <- as.Date(fechamento$data)
-  fit_01 <- ugarchfit(spec, fechamento$fechamento[-3391], solver = 'hybrid')
+  retorno[nrow(retorno)+1,1] <- previsao_antes[nrow(previsao_antes),1]
+  retorno[nrow(retorno),2] <- as.Date(previsao_antes[nrow(previsao_antes),3])
+  retorno[,3] <- as.Date(retorno$data)
+  fit_01 <- ugarchfit(spec, retorno$retorno[-3391], solver = 'hybrid')
   
-  previsao <- ugarchforecast(fit_01, fechamento[-3391],
+  previsao <- ugarchforecast(fit_01, retorno[-3391],
                              n.ahead = nrow(previsao_antes)+1)
   previsao_antes[,3]<-as.Date(previsao_antes[,3])
   previsao_antes[nrow(previsao_antes)+1,1] <- previsao@forecast[["seriesFor"]][nrow(previsao_antes)]
   previsao_antes[nrow(previsao_antes),2] <- previsao@forecast[["sigmaFor"]][nrow(previsao_antes)]
-  previsao_antes[nrow(previsao_antes),3] <- fechamento$data[nrow(fechamento)]+1
+  previsao_antes[nrow(previsao_antes),3] <- retorno$data[nrow(retorno)]+1
 }
 
 if (nrow(previsao_antes) == 0){
-  fit_01 <- ugarchfit(spec, fechamento$fechamento, solver = 'hybrid')
+  fit_01 <- ugarchfit(spec, retorno$retorno, solver = 'hybrid')
   
-  previsao <- ugarchforecast(fit_01, fechamento,
+  previsao <- ugarchforecast(fit_01, retorno,
                              n.ahead = 1)
   previsao_antes[,3]<-as.Date(previsao_antes[,3])
   previsao_antes[nrow(previsao_antes)+1,1] <- previsao@forecast[["seriesFor"]]
   previsao_antes[nrow(previsao_antes),2] <- previsao@forecast[["sigmaFor"]]
-  previsao_antes[nrow(previsao_antes),3] <- fechamento$data[nrow(fechamento)]+1
+  previsao_antes[nrow(previsao_antes),3] <- retorno$data[nrow(retorno)]+1
 }
 
 write_csv(previsao_antes,paste0('data/','previsao','.csv'))   
