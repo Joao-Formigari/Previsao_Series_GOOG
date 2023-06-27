4cb37fec55435aed9ff381c165110f1eb7ebf645
Joao-Formigari <137228301+Joao-Formigari@users.noreply.github.com>
2023-06-26 21:23:53 -0300

    Update previsao.R

--- R/previsao.R
+++ R/previsao.R
@@ -7,38 +7,40 @@ nome_acao <- "GOOG"   # Codigo no Yahoo Finance
 data_ini  <- "2010-01-01" # Data de inicio
 data_fim <- Sys.Date()
 precos <- yf_get(tickers = nome_acao, first_date = data_ini, last_date = data_fim)
-retorno <- as.data.frame(list(precos$ret_adjusted_prices[-1], precos$ref_date[-1]))
-colnames(retorno) <- c("retorno","data")
+retornos <- as.data.frame(list(precos$ret_adjusted_prices[-1], precos$ref_date[-1]))
+colnames(retornos) <- c("retornos","data")
 
-spec <- ugarchspec(mean.model = list(armaOrder = c(2, 1), include.mean = FALSE),
-                     variance.model = list(model = 'sGARCH', garchOrder = c(1, 1)),
-                     distribution = 'norm')
-  
+spec <- ugarchspec(mean.model = list(armaOrder = c(3, 3), include.mean = FALSE),
+                   variance.model = list(model = 'sGARCH', garchOrder = c(1, 1)),
+                   distribution = 'std')
+
+#acf((fit4@fit$residuals/fit4@fit$sigma)^2)
+
 previsao_antes <- read.csv("https://raw.githubusercontent.com/Joao-Formigari/Teste/master/data/previsao.csv")
 
 if (nrow(previsao_antes) > 0){
-  retorno[nrow(retorno)+1,1] <- previsao_antes[nrow(previsao_antes),1]
-  retorno[nrow(retorno),2] <- as.Date(previsao_antes[nrow(previsao_antes),3])
-  retorno[,3] <- as.Date(retorno$data)
-  fit_01 <- ugarchfit(spec, retorno$retorno[-3391], solver = 'hybrid')
+  retornos[nrow(retornos)+1,1] <- previsao_antes[nrow(previsao_antes),1]
+  retornos[nrow(retornos),2] <- as.Date(previsao_antes[nrow(previsao_antes),3])
+  retornos[,3] <- as.Date(retornos$data)
+  fit_01 <- ugarchfit(spec, retornos$retornos, solver = 'hybrid')
   
-  previsao <- ugarchforecast(fit_01, retorno[-3391],
-                             n.ahead = nrow(previsao_antes)+1)
-  previsao_antes[,3]<-as.Date(previsao_antes[,3])
-  previsao_antes[nrow(previsao_antes)+1,1] <- previsao@forecast[["seriesFor"]][nrow(previsao_antes)]
-  previsao_antes[nrow(previsao_antes),2] <- previsao@forecast[["sigmaFor"]][nrow(previsao_antes)]
-  previsao_antes[nrow(previsao_antes),3] <- retorno$data[nrow(retorno)]+1
-}
-
-if (nrow(previsao_antes) == 0){
-  fit_01 <- ugarchfit(spec, retorno$retorno, solver = 'hybrid')
-  
-  previsao <- ugarchforecast(fit_01, retorno,
+  previsao <- ugarchforecast(fit_01, retornos,
                              n.ahead = 1)
   previsao_antes[,3]<-as.Date(previsao_antes[,3])
   previsao_antes[nrow(previsao_antes)+1,1] <- previsao@forecast[["seriesFor"]]
   previsao_antes[nrow(previsao_antes),2] <- previsao@forecast[["sigmaFor"]]
-  previsao_antes[nrow(previsao_antes),3] <- retorno$data[nrow(retorno)]+1
+  previsao_antes[nrow(previsao_antes),3] <- retornos$data[nrow(retornos)]+1
+}
+
+if (nrow(previsao_antes) == 0){
+  fit_01 <- ugarchfit(spec, retornos$retornos, solver = 'hybrid')
+  
+  previsao <- ugarchforecast(fit_01, retornos,
+                             n.ahead = 1)
+  previsao_antes[,3]<-as.Date(previsao_antes[,3])
+  previsao_antes[nrow(previsao_antes)+1,1] <- previsao@forecast[["seriesFor"]]
+  previsao_antes[nrow(previsao_antes),2] <- previsao@forecast[["sigmaFor"]]
+  previsao_antes[nrow(previsao_antes),3] <- retornos$data[nrow(retornos)]+1
 }
 
 write_csv(previsao_antes,paste0('data/','previsao','.csv'))   
